2e334b2c98e651dda4dd477ca88e13ad254d9f67
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-19 23:02:45 +0530

    updated

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,8 @@
 from pydantic import EmailStr
 from sqlalchemy.orm import Session
 from app.model import User_list
+from app.schema import UserInDb
+from app.hashpassword import get_password_hash
 
 
 def findByEmail(email: EmailStr, db: Session):
@@ -9,3 +11,13 @@ def findByEmail(email: EmailStr, db: Session):
 
 def findByusername(username: str, db: Session):
     return db.query(User_list).filter(User_list.username == username).first()
+
+
+def create_user(db: Session, user: UserInDb):
+    hashPassword = get_password_hash(user.password)
+    db_user = User_list(
+        email=user.email, username=user.username, password=hashPassword)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
